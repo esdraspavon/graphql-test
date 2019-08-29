622b9b7bc75fcd0fbfdfc4ed246f691129599d56
Esdras Pavon <esdras.pavon@devlights.com>
2019-08-29 11:02:38 -0300

    Custom Types

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 "use strict";
 
-const { graphql, buildSchema } = require("graphql");
+const { buildSchema } = require("graphql");
 const express = require("express");
 const gqlMiddleware = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
 
 const app = express();
 const port = process.env.port || 3000;
 
 // definiendo el esquema
 
-const schema = buildSchema(`
-  type Query {
-    "Retorna un saludo al mundo"
-    hello: String
-    "Retorna un saludo a todos"
-    saludo: String
-  }
-`);
-
-// Configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return "Hola Mundo";
-  },
-  saludo: () => {
-    return "Hola a todos";
-  }
-};
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
 
 // Ejecutar el  query hello
 // graphql(schema, "{ hello, saludo }", resolvers).then(data => {
