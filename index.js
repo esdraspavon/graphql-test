a5721552faf0386770ccd032b86e47b3e134af45
Esdras Pavon <esdras.pavon@devlights.com>
2019-08-29 10:36:33 -0300

    Sirviendo el API en la web

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 "use strict";
 
 const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+
+const app = express();
+const port = process.env.port || 3000;
 
 // definiendo el esquema
 
 const schema = buildSchema(`
   type Query {
-    hello: String,
+    "Retorna un saludo al mundo"
+    hello: String
+    "Retorna un saludo a todos"
     saludo: String
   }
 `);
@@ -22,6 +29,19 @@ const resolvers = {
 };
 
 // Ejecutar el  query hello
-graphql(schema, "{ hello, saludo }", resolvers).then(data => {
-  console.log(data);
+// graphql(schema, "{ hello, saludo }", resolvers).then(data => {
+//   console.log(data);
+// });
+
+app.use(
+  "/api",
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
 });
