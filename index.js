30ed44f81d7858ce68d0f4d95798f11d183bc1c2
Esdras Pavon <pavonesdras@gmail.com>
2019-08-30 18:29:05 -0300

    Creando el tipo estudiante

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict'
 
-require("dotenv").config();
-const { makeExecutableSchema } = require("graphql-tools");
-const express = require("express");
-const gqlMiddleware = require("express-graphql");
-const { readFileSync } = require("fs");
-const { join } = require("path");
-const resolvers = require("./lib/resolvers");
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
 const typeDefs = readFileSync(
-  join(__dirname, "lib", "schema.graphql"),
-  "utf-8"
-);
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 // Ejecutar el  query hello
 // graphql(schema, "{ hello, saludo }", resolvers).then(data => {
@@ -24,14 +24,14 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 // });
 
 app.use(
-  "/api",
+  '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
     graphiql: true
   })
-);
+)
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`);
-});
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
