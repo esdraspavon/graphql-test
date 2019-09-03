a6ed6ecbdfa85f0c47bc4123b9ff6512c060a0d6
Esdras Pavon <pavonesdras@gmail.com>
2019-09-03 00:48:01 -0300

    Preparando API para producción

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,6 +11,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el esquema
 const typeDefs = readFileSync(
@@ -23,12 +25,14 @@ const schema = makeExecutableSchema({ typeDefs, resolvers })
 //   console.log(data);
 // });
 
+app.use(cors())
+
 app.use(
   '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
   })
 )
 
